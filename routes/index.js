657eb633ae1caf98136168dd603711395e71f40a
codeBean76 <yuyaebean@gmail.com>
2018-10-06 23:51:10 +0900

    Make company introduce page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/company', (req, res, next) => {
+  res.render('company', { title: 'Company - A One' });
 });
 
 module.exports = router;
