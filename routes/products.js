52bc3a28634b1a95ce0b606bfbb04a0ab6cce45b
codeBean76 <codeBean76@gmail.com>
2019-03-01 04:44:43 +0900

    Make nav in product page
    Fix security problems in product router.

--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../models/category');
 const Product = require('../models/product');
 const isAuth = require('../lib/isAuth');
 
 // products list page
 router.get('/', async (req, res, next) => {
   const products = await Product.find();
-  res.render('product/index', { title: 'Product - A One', product: products });
+  const categories = await Category.find().populate('children');
+  res.render('product/index', { title: 'Product - A One', product: products, category: categories });
 });
 
 // product register page
-router.get('/new', (req, res, next) => {
+router.get('/new', isAuth, (req, res, next) => {
   res.render('product/new', { title: 'New Product - A One '});
 });
 
@@ -20,7 +22,7 @@ router.get('/:id', async (req, res, next) => {
   res.render('product/show', { title: product.name + ' - A One', product: product });
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', isAuth, async (req, res, next) => {
   const product = new Product({
     name: req.body.product_name,
     thumbnail: req.body.thumbnail, // TODO: This is temperary measure. Need to fix.
